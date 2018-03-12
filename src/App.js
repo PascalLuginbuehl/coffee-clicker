4de25478e4d562a7f4778c7c68a760c111aac6a6
Pascal Luginbühl <spargelplays@gmail.com>
2018-03-12 10:41:13 +0100

    Added scroll buttons to upgrades

--- src/App.js
+++ src/App.js
@@ -6,35 +6,63 @@ import { calcCoffeeSpeedFromUpgrades } from './reducers/calcCoffeeSpeedFromUpgra
 
 
 class App extends Component {
+  constructor() {
+    super()
+
+
+    this.state = {
+      showInWindow: 4,
+      scrollPage: 1,
+    }
+  }
+
+  scrollDown() {
+    if (this.props.availableUpgrades.length > this.state.scrollPage * this.state.showInWindow) {
+      this.setState({scrollPage: this.state.scrollPage + 1})
+    }
+  }
+
+  scrollUp() {
+    console.log(this.state.scrollPage)
+    if (1 < this.state.scrollPage) {
+      this.setState({ scrollPage: this.state.scrollPage - 1 })
+    }
+  }
+
   render() {
+    const state = this.state
+    const props = this.props
+
     return (
       <div className="App">
        <main>
-        <div class="main">
+        <div className="main">
           <div>
             <h2>Counter</h2>
-            <p>{Math.floor(this.props.coffeeCounter)}</p>
-              <p>U will get {calcCoffeeSpeedFromUpgrades(this.props.Upgrades)}/sec</p>
+            <p>{Math.floor(props.coffeeCounter)}</p>
+              <p>U will get {calcCoffeeSpeedFromUpgrades(props.Upgrades)}/sec</p>
           </div>
 
-          <div onClick={this.props.onCoffeeClick}>
-            <img class="coffee-container" src="click-this-coffee.png" alt="Click This!"/>
+          <div onClick={props.onCoffeeClick}>
+            <img className="coffee-container" src="click-this-coffee.png" alt="Click This!"/>
           </div>
           </div>
-          <div class="aside">
-            <div class="upgrade-container">
-              {this.props.availableUpgrades.map(upgrade => (
-                <div onClick={() => this.props.onBuyCoffee(upgrade.name)} key={upgrade.name} className={upgrade.price > this.props.coffeeCounter ? "upgrade-item not-buyable" : "upgrade-item" }>
-                  <div class="left">
+          <div className="aside">
+            <div className="upgrade-container">
+              <div onClick={this.scrollUp.bind(this)}>Öp</div>
+              {props.availableUpgrades.filter((e, i) => i < state.scrollPage * state.showInWindow && i >= (state.scrollPage - 1) * state.showInWindow).map(upgrade => (
+                <div onClick={() => props.onBuyCoffee(upgrade.name)} key={upgrade.name} className={upgrade.price > props.coffeeCounter ? "upgrade-item not-buyable" : "upgrade-item" }>
+                  <div className="left">
                     <h3>{upgrade.name}</h3>
                     <p>+{upgrade.bonus} pro Sekunde</p>
                   </div>
-                  <div class="right">
-                    <h3>{this.props.Upgrades.find(e => e.name === upgrade.name) ? this.props.Upgrades.find(e => e.name === upgrade.name).count : 0}x</h3>
+                  <div className="right">
+                    <h3>{props.Upgrades.find(e => e.name === upgrade.name) ? props.Upgrades.find(e => e.name === upgrade.name).count : 0}x</h3>
                     <p>Preis: {upgrade.price}.-</p>
                   </div>
                 </div>
               ))}
+              <div onClick={this.scrollDown.bind(this)}>döwn</div>
             </div>
           </div>
         </main>
